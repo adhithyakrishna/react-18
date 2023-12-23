18a40a99eed31b16149d436a830bcf722c2c4312
Adhi <adhithyakrishna.kovaisrinivasan@nordstrom.com>
2023-12-23 14:25:35 -0800

    Day 1 - Session 1 - (Components and Props)

--- tutorial-app/src/index.js
+++ tutorial-app/src/index.js
@@ -1,58 +1,80 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// function Greeting() {
-//   return <h3> My First Component </h3>;
-// }
+import './index.css'
 
-// function Greeting() {
-//   return React.createElement('h2', {}, 'hello World')
-// }
+const firstBook = {
+  author: 'Rebecca Yarros',
+  title: 'Fourth Wing',
+  img: './images/fourth-wing.jpg',
+}
 
-// function Greeting() {
-//   return React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h2', {}, 'hello World')
-//   )
-// }
+const secondBook = {
+  author: 'Jordan Moore',
+  title: 'Interesting facts for curious minds',
+  img: './images/curious-minds.jpg',
+}
 
 /*
-function Greeting() {
+  Changes made to one component would automatically render it to other components
+ */
+const BookList = () => {
   return (
-    <React.Fragment>
-      <div>1st child</div>
-      <div>2nd child</div>
-    </React.Fragment>
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
   )
-}*/
+}
+
+/*
+  The props will be only displayed if it is provided
+*/
+const Book = ({ img, title, author }) => {
+  // console.log(props)
+  // const { img, title, author } = props
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      {/* you can only pass an expression and the expression returns a value */}
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+/*CDN*/
+// const Image = () => (
+//   <img
+//     src="https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/91n7p-j5aqL._AC_UL900_SR900,600_.jpg"
+//     alt="Fourth Wing"
+//   />
+// )
 
 /* 
-  Notice the camelCase in className as well as readOnly
-  All elements that do not have closing tag, we need to self close them.
+  Public folder 
+  http://localhost:3000/images/fourth-wing.jpg
+  This is not a popular option is because these assets are not optimized 
 */
-// function Greeting() {
+// const Image = () => <img src="./images/fourth-wing.jpg" alt="Fourth Wing" />
+
+// const Title = () => <h2>Book title</h2>
+// const Author = () => {
 //   return (
-//     <div className="helloworld" tabIndex={1}>
-//       <input readOnly={true} id="name" />
-//     </div>
+//     // inline styles
+//     <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+//       Rebecca
+//     </h4>
 //   )
 // }
-
-/* How to nest components */
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  )
-}
-const Person = () => <h2>John doe</h2>
-const Message = () => {
-  return <p> This is my message </p>
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting />)
+root.render(<BookList />)
