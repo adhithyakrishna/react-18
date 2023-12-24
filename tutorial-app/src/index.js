4bd84c7e5f8912a26eae787749fad82ab3b0add2
Adhi <adhithyakrishna.kovaisrinivasan@nordstrom.com>
2023-12-23 22:30:13 -0800

    Day 1 - Components and props basic check in

--- tutorial-app/src/index.js
+++ tutorial-app/src/index.js
@@ -3,50 +3,60 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
-const firstBook = {
-  author: 'Rebecca Yarros',
-  title: 'Fourth Wing',
-  img: './images/fourth-wing.jpg',
-}
-
-const secondBook = {
-  author: 'Jordan Moore',
-  title: 'Interesting facts for curious minds',
-  img: './images/curious-minds.jpg',
-}
+const books = [
+  {
+    author: 'Rebecca Yarros',
+    title: 'Fourth Wing',
+    img: './images/fourth-wing.jpg',
+    id: 1,
+  },
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting facts for curious minds',
+    img: './images/curious-minds.jpg',
+    id: 2,
+  },
+]
 
 /*
   Changes made to one component would automatically render it to other components
  */
+
+/*
+  Objects cannot be directly rendered in JSX   
+   */
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        // Destructure it and pass it to the book component
+        // index is not advisable to be used as a key prop, unless we are sure that the list length is not going to change
+        // const { img, title, author, id } = book
+
+        // return <Book book={book} key={book.id}></Book>
+        return <Book {...book} key={book.id} />
+      })}
     </section>
   )
 }
 
 /*
+  We use 
+*/
+/*
   The props will be only displayed if it is provided
 */
-const Book = ({ img, title, author }) => {
+const Book = (props) => {
   // console.log(props)
-  // const { img, title, author } = props
+  // const { img, title, author, children } = props.book
+  const { img, title, author, children } = props
   return (
     <article className="book">
       <img src={img} alt={title} />
       {/* you can only pass an expression and the expression returns a value */}
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {children}
     </article>
   )
 }
